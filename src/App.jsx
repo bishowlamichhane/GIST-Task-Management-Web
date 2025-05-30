d60e896bb220f0fd928729c57efe0222d678f357
bishowlamichhane <lamichhanebisho4545@gmail.com>
2025-05-30 18:21:57 +0545

    framer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "./components/layout";
 import Dashboard from "./components/dashboard";
 import Calendar from "./components/calendar";
@@ -15,39 +15,49 @@ import Login from "./components/login";
 import Register from "./components/register";
 import useStore from "./store/store";
 import { ThemeProvider } from "./components/theme-provider";
-
+import { motion } from "framer-motion";
 const App = () => {
   const { loggedIn, initialAuth } = useStore();
+  const [isVisible, setIsVisible] = useState(false);
 
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
   useEffect(() => {
     initialAuth();
   }, [initialAuth]);
 
   return (
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          {/* Protected routes using the Layout */}
-          <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/tasks" element={<Tasks />} />
-            {/* Add more routes as needed */}
-          </Route>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isVisible ? 1 : 0 }}
+      transition={{ duration: 1.0 }}
+    >
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            {/* Protected routes using the Layout */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/tasks" element={<Tasks />} />
+              {/* Add more routes as needed */}
+            </Route>
 
-          {/* Public routes */}
-          {/* <Route
+            {/* Public routes */}
+            {/* <Route
             path="/login"
             element={loggedIn ? <Navigate to="/" /> : <Login />}
           /> */}
-          {/* <Route
+            {/* <Route
             path="/register"
             element={loggedIn ? <Navigate to="/" /> : <Register />}
           />
            */}
-        </Routes>
-      </Router>
-    </ThemeProvider>
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </motion.div>
   );
 };
 
